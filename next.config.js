5dcef369ab6d3a7a35ede62c7ec50d606253ec15
Daniel <daniel.russi@hotmail.com>
2022-12-14 13:33:22 -0500

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ["cdn.sanity.io","cdn-icons-png.flaticon.com"] },
+  images: { domains: ["cdn.sanity.io", "cdn-icons-png.flaticon.com"] },
+  api: { domains: ["https://nv2022141202-1rktq8gj.uc.gateway.dev/service"] },
 };
 
 module.exports = nextConfig;
